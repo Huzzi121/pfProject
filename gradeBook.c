d787c85dcc0cdf7f139f0f8bba5ab9c9724aafc3
huzaifa <huzaifakashif2003@gmail.com>
2024-01-06 15:42:21 +0500

    struct created and maxsub and max std defined

---
+++ gradeBook.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define maxsub 5    //defined the number of subject early 
+#define maxstd 30   //defined the number of max students early
+
+
+struct Student
+{
+    char name[25];
+    double grades[maxsub];
+};
+
